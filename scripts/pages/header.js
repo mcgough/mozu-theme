027b593d9e945964df01d5cf54e99b7deadc3b81
unknown <dmcgough@dmcgough.flyfish.wan>
2016-02-01 15:48:51 -0800

    mobile header added

--- scripts/pages/header.js
+++ scripts/pages/header.js
@@ -1,13 +1,10 @@
 define(['jquery'], function($) {
   if(window.innerWidth > 550) {
-
     var $headerNav = $('.minimal');
-
     $(document).scroll(function(){
       var offset = 130;
       var scroll = $(document).scrollTop();
       if(scroll >= offset) {
-        console.log('scrolled');
         $('body').addClass('sticky');
         return $headerNav.addClass('fixed');
       } else {
